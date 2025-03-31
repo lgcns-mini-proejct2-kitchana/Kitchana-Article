aa5121ff762126a5492ae83a87e687e3874966ea
Tae Jeong Kim <oganesson12@hufs.ac.kr>
2025-03-31 23:50:42 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,11 +8,27 @@
 
 # docker-compose.yml 파일이 위치한 디렉토리 (실제 경로로 수정)
 COMPOSE_DIR="/home/ec2-user/inner"
+REPO="kitchana/article"
+REGION="ap-southeast-2"
 
 cd "$COMPOSE_DIR" || { echo "Compose directory not found"; exit 1; }
 
-# 최신 article 이미지 가져오기
-docker-compose pull article
+# ECR에 있는 최신 이미지 태그 가져오기
+LATEST_TAG=$(aws ecr describe-images \
+  --repository-name $REPO \
+  --region $REGION \
+  --query 'sort_by(imageDetails,& imagePushedAt)[-1].imageTags[0]' \
+  --output text)
+
+# 최신 이미지 태그를 이용해서 article 이미지 가져오기
+docker pull ${AWS_ECR_URI}/$REPO:$LATEST_TAG
+
+echo "새로운 이미지 pull 성공???"
+
+# .env 파일에 새로운 LATEST_TAG 넣어주기
+grep -q '^ARTICLE_TAG=' .env \
+  && sed -i "s/^ARTICLE_TAG=.*/ARTICLE_TAG=$LATEST_TAG/" .env \
+  || echo "ARTICLE_TAG=$LATEST_TAG" >> .env
 
 # article 컨테이너만 강제 재생성 (기존 컨테이너 종료 및 삭제 후 새 컨테이너 실행)
 docker-compose up -f docker-compose-inner.yml -d --no-deps --force-recreate article 
